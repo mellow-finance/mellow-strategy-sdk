631ebdb666179744683facf63cf0e5e1f5f8436c
Alexey Karasev <karasev.alexey@gmail.com>
2021-06-30 15:35:27 +0300

    updates

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 from enum import Enum
 from io import StringIO
-import boto3
 import pandas as pd
 import os
 
@@ -8,6 +7,7 @@ import os
 class Pool(Enum):
     ETHWBTC = "ETHWBTC"
     ETHUSDC = "ETHUSDC"
+    USDTUSDC = "USDTUSDC"
 
 
 class Frequency(Enum):
@@ -22,32 +22,54 @@ class Frequency(Enum):
 
 
 class RawData:
-    def __init__(self, pool: Pool, freq: Frequency):
+    def __init__(self, pool: Pool):
         self._pool = pool
-        self._freq = freq
-        self.burns = self._fetch_pool_data("burns")
-        self.mints = self._fetch_pool_data("mints")
-        self.swaps = self._fetch_pool_data("swaps")
-
-    def _fetch_pool_data(self, kind):
-        aws_id = os.getenv("AWS_ACCESS_KEY_ID")
-        aws_secret = os.getenv("AWS_SECRET_ACCESS_KEY")
-
-        client = boto3.client(
-            "s3", aws_access_key_id=aws_id, aws_secret_access_key=aws_secret
+        print(f"Downloading swaps")
+        self.swaps = pd.read_csv(
+            self._get_download_url("swaps"),
+            converters={
+                "block_time": int,
+                "block_number": int,
+                "sqrt_price_x96": int,
+                "amount0": int,
+                "amount1": int,
+                "liquidity": int,
+            },
+            parse_dates=["block_time"],
         )
+        self.swaps.index = pd.to_datetime(self.swaps["block_time"], unit="s")
+        print(f"Downloading burns")
+        self.burns = pd.read_csv(
+            self._get_download_url("burns"),
+            converters={
+                "block_time": int,
+                "block_number": int,
+                "tick_lower": int,
+                "tick_upper": int,
+                "amount": int,
+                "amount0": int,
+                "amount1": int,
+            },
+        )
+        self.burns.index = pd.to_datetime(self.burns["block_time"], unit="s")
+        print(f"Downloading mints")
+        self.mints = pd.read_csv(
+            self._get_download_url("mints"),
+            converters={
+                "block_time": int,
+                "block_number": int,
+                "tick_lower": int,
+                "tick_upper": int,
+                "amount": int,
+                "amount0": int,
+                "amount1": int,
+            },
+        )
+        self.mints.index = pd.to_datetime(self.mints["block_time"], unit="s")
+        print("Done")
 
-        bucket_name = os.getenv("AWS_DATA_BUCKET") or "mellow-uni-data"
-
-        object_key = f"{kind}-{self._pool.value}.csv"
-        csv_obj = client.get_object(Bucket=bucket_name, Key=object_key)
-        body = csv_obj["Body"]
-        csv_string = body.read().decode("utf-8")
-
-        return pd.read_csv(StringIO(csv_string))
-
-
-data = RawData(Pool.ETHWBTC, Frequency.HOUR)
-print(data.burns)
-print(data.mints)
-print(data.swaps)
+    def _get_download_url(self, kind):
+        host = (
+            os.getenv("AWS_DATA_HOST") or "mellow-uni-data.s3.us-east-2.amazonaws.com"
+        )
+        return f"https://{host}/{kind}-{self._pool.value}.csv"
--- utils/export.py
+++ utils/export.py
@@ -8,8 +8,9 @@ db = os.getenv("DB")
 password = os.getenv("PASSWORD")
 
 pools = {
+    "ETHWBTC": "0xCBCdF9626bC03E24f779434178A73a0B4bad62eD",
     "ETHUSDC": "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8",
-    "ETHWBTC": "0x7858E59e0C01EA06Df3aF3D20aC7B0003275D4Bf",
+    "USDTUSDC": "0x7858E59e0C01EA06Df3aF3D20aC7B0003275D4Bf",
 }
 
 events = ["burns", "mints", "swaps"]
@@ -18,7 +19,7 @@ for pool, address in pools.items():
     for event in events:
         name = f"{event}-{pool}.csv"
         print(f"Exporting {name}")
-        bashCommand = f'mysql -h {host} -u{user} -p{password} {db} -e \'select * from {event} WHERE pool="{address}"\' | sed \'s/\\t/","/g;s/^/"/;s/$/"/;s/\\n//g\' > data/{name}'
+        bashCommand = f'mysql -h {host} -u{user} -p{password} {db} -e \'select * from {event} WHERE pool="{address}" ORDER BY block_time\' | sed \'s/\\t/","/g;s/^/"/;s/$/"/;s/\\n//g\' > data/{name}'
         print(bashCommand)
         process = subprocess.Popen(["bash", "-c", bashCommand])
         process.wait()
