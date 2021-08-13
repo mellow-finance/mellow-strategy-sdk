ad6f3467ebaef3b5251f16587483cd102c287b54
ksetherg <ksetherg@gmail.com>
2021-08-13 15:01:23 +0300

    Implementing permanent loss realisation

--- strategy/portfolio.py
+++ strategy/portfolio.py
@@ -112,6 +112,7 @@ class Position(AbstractPosition):
         self._bi_x = 0
         self._fees0 = 0
         self._fees1 = 0
+        self.pl = 0
 
     @property
     def fees0(self) -> float:
@@ -184,12 +185,13 @@ class Position(AbstractPosition):
         :param fee_percent: Fee for the current pool, e.g. 0.003
         :return: Liquidity added
         """
+        assert np.isclose(c*x, y, atol=1e-6), 'Token amounts do not match.'
         x_c, y_c = align_for_liquidity(x, y, self._a, self._b, c, fee_percent)
         delta_l = liq(x_c, y_c, self._a, self._b, c)
         self._l += delta_l
         if self._bi_x == 0 and self._bi_y == 0:
-            self._bi_x = x_c
-            self._bi_y = y_c
+            self._bi_x += x_c
+            self._bi_y += y_c
         return delta_l
 
     def withdraw(self, c: float, l: float) -> Tuple[float, float]:
@@ -200,8 +202,12 @@ class Position(AbstractPosition):
         :param l: Amount of liquidity to withdraw
         :return: amount of ``Y`` token actually withdrawn
         """
+        assert l <= self._l, 'Too mush liquidity to withdraw.'
         x, y = xy(l, self._a, self._b, c)
+        self._bi_x -= x
+        self._bi_y -= y
         self._l -= l
+        self.pl += self.il(c)
         return x, y
 
     def y(self, c: float) -> float:
