331b2d65aae283ca0d3c3215f564fab758068db2
Alexander Nikolaev <aenik97@gmail.com>
2022-03-02 01:19:33 +0300

    rename folder

--- tests_unittests/UniswapLiquidityAligner_test.py
+++ tests_unittests/UniswapLiquidityAligner_test.py
@@ -5,7 +5,7 @@
         liq_to_optimal_xy - Yes
         check_xy_is_optimal - Yes
 
-    cd tests_pytest and python -m unittest UniswapLiquidityAligner_test.py
+    cd tests_unittests and python -m unittest UniswapLiquidityAligner_test.py
 """
 
 import sys
