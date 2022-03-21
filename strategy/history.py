Fix IL calculation

strategy/history.py
@@ -5,6 +5,7 @@ import datetime
 from typing import Hashable
 import typing as tp
 
+
 class PortfolioHistory:
     """
     | ``PortfolioHistory`` accumulate snapshots and can calculate stats over time from snapshots.
@@ -65,16 +66,16 @@ class PortfolioHistory:
             dataframe consisting of two columns total_il_x, total_il_y
         """
         # log.info('Starting to calculate ils')
-        il_to_x_cols = [col for col in df.columns if 'il_x' in col]
-        il_to_y_cols = [col for col in df.columns if 'il_y' in col]
+        il_to_x_cols = [col for col in df.columns if 'il_to_x' in col]
+        il_to_y_cols = [col for col in df.columns if 'il_to_y' in col]
 
         if il_to_x_cols:
-            df_x = df[il_to_x_cols].fill_null('forward').fill_null(0).sum(axis=1).alias('total_il_x')
-            df_y = df[il_to_y_cols].fill_null('forward').fill_null(0).sum(axis=1).alias('total_il_y')
+            df_x = df[il_to_x_cols].fill_null('forward').sum(axis=1).alias('total_il_to_x')
+            df_y = df[il_to_y_cols].fill_null('forward').sum(axis=1).alias('total_il_to_y')
         else:
             data = [0] * df.shape[0]
-            df_x = pl.Series('total_il_x', data, dtype=pl.Float64)
-            df_y = pl.Series('total_il_y', data, dtype=pl.Float64)
+            df_x = pl.Series('total_il_to_x', data, dtype=pl.Float64)
+            df_y = pl.Series('total_il_to_y', data, dtype=pl.Float64)
         return pl.DataFrame([df_x, df_y])
 
     def calculate_fees(self, df: pl.DataFrame) -> pl.DataFrame:
@@ -125,8 +126,6 @@ class PortfolioHistory:
             (pl.col('total_value_x') * pl.col('price') + pl.col('total_value_y')).alias('total_value_to_y'),
             (pl.col('total_fees_x') + pl.col('total_fees_y') / pl.col('price')).alias('total_fees_to_x'),
             (pl.col('total_fees_x') * pl.col('price') + pl.col('total_fees_y')).alias('total_fees_to_y'),
-            (pl.col('total_il_x') + pl.col('total_il_y') / pl.col('price')).alias('total_il_to_x'),
-            (pl.col('total_il_x') * pl.col('price') + pl.col('total_il_y')).alias('total_il_to_y'),
             (pl.col('total_value_x').first() + pl.col('total_value_y').first() / pl.col('price')).alias('hold_to_x'),
             (pl.col('total_value_x').first() * pl.col('price') + pl.col('total_value_y').first()).alias('hold_to_y'),
             (pl.col('total_value_x') + pl.col('total_value_y') / pl.col('price').last()).alias('vpn_value'),
@@ -178,8 +177,7 @@ class PortfolioHistory:
         ])
         return df_returns
 
-    def calculate_apy_for_col(self, df: pl.DataFrame, from_col: str, to_col: str,
-                              calculate_full: bool = False) -> pl.DataFrame:
+    def calculate_apy_for_col(self, df: pl.DataFrame, from_col: str, to_col: str) -> pl.DataFrame:
         """
             Calculate APY for metric
 
@@ -187,27 +185,16 @@ class PortfolioHistory:
             df: dataframe with metrics
             from_col: metric column name
             to_col: name for new column with metric APY
-            calculate_full:
-                | True: calculate APY from first day to last
-                | False: calculate APY from first day to current
 
         Returns:
             dataframe consisting of 'to_col' column
         """
 
-        if calculate_full:
-            df_performance = df.select([
-                (pl.col(from_col) / pl.col(from_col).first()).alias('performance'),
-                ((pl.col('timestamp').last() - pl.col('timestamp').first()).dt.days()).alias('days')
-            ])
-        else:
-            df_performance = df.select([
+        df_performance = df.select([
                 (pl.col(from_col) / pl.col(from_col).first()).alias('performance'),
                 ((pl.col('timestamp') - pl.col('timestamp').first()).dt.days()).alias('days')
             ])
-
         df_apy = df_performance.apply(lambda x: 100 * (x[0] ** (365 / x[1]) - 1) if x[1] != 0 else 0.)
-
         df_apy = df_apy.rename({"apply": to_col})
         return df_apy
 
@@ -265,14 +252,10 @@ class RebalanceHistory:
         Returns:
             Data frame of porfolio actions, except None actions.
         """
-        df = (
-            pl.DataFrame(self.rebalances)
-            .drop_nulls()
-            .with_column(
-                pl.col('timestamp')
-                .cast(pl.Datetime)
-            )
-        )
+        df = pl.DataFrame([
+            pl.Series('timestamp', [x['timestamp'] for x in self.rebalances]),
+            pl.Series('rebalance', [x['rebalance'] for x in self.rebalances], dtype=pl.Utf8),
+        ]).drop_nulls().with_column(pl.col('timestamp').cast(pl.Datetime))
         return df
 
 
@@ -325,25 +308,5 @@ class UniPositionsHistory:
     #     Returns:
     #         Uniswap positions history data frame.
     #     """
-    #     prices = swaps_df[['price']]
-    #     prices['covered'] = np.nan
-    #
-    #     intervals = self.to_df()
-    #     prices_sliced = prices.loc[intervals.index]['price']
-    #
-    #     min_bound = intervals.loc[:, intervals.columns.get_level_values(level=1) == 'lower_bound']
-    #     max_bound = intervals.loc[:, intervals.columns.get_level_values(level=1) == 'upper_bound']
-    #
-    #     min_bound.columns = list(min_bound.columns.droplevel(1))
-    #     max_bound.columns = list(max_bound.columns.droplevel(1))
-    #
-    #     min_mask = min_bound.lt(prices_sliced, axis=0).any(axis=1)
-    #     max_mask = max_bound.gt(prices_sliced, axis=0).any(axis=1)
-    #
-    #     final_mask = min_mask & max_mask
-    #
-    #     prices.loc[intervals.index, 'covered'] = final_mask
-    #     prices.loc[:, 'covered'] = prices['covered'].fillna(False)
-    #     coverage = prices['covered'].sum() / prices.shape[0]
-    #
+    #     pass
     #     return coverage

strategy/strategies.py
@@ -43,7 +43,7 @@ class Hold(AbstractStrategy):
         self.prev_gain_date = None
 
     def rebalance(self, *args, **kwargs):
-        timestamp = kwargs['timestamp']
+        timestamp = kwargs['record']['timestamp']
         portfolio = kwargs['portfolio']
 
         if self.prev_gain_date is None:
@@ -81,10 +81,9 @@ class UniV3Passive(AbstractStrategy):
         self.rebalance_cost = rebalance_cost
 
     def rebalance(self, *args, **kwargs) -> str:
-        timestamp = kwargs['timestamp']
-        row = kwargs['row']
+        record = kwargs['record']
         portfolio = kwargs['portfolio']
-        price_before, price = row['price_before'], row['price']
+        price_before, price = record['price_before'], record['price']
 
         is_rebalanced = None
 
@@ -105,3 +104,125 @@ class UniV3Passive(AbstractStrategy):
         univ3_pos.deposit(x=x_uni_aligned, y=y_uni_aligned, price=price)
         return univ3_pos
 
+
+class StrategyByAddress(AbstractStrategy):
+    def __init__(self,
+                 address,
+                 pool: Pool,
+                 rebalance_cost: float,
+                 name: str = None,
+                 ):
+        super().__init__(name)
+
+        self.address = address
+        self.decimal_diff = -pool.decimals_diff
+        self.fee_percent = pool.fee.percent
+        self.rebalance_cost = rebalance_cost
+
+    def rebalance(self, *args, **kwargs):
+        # prepare data for strategy
+        is_rebalanced = None
+
+        record = kwargs['record']
+        portfolio = kwargs['portfolio']
+        event = record['event']
+
+        if event == 'mint':
+            if record['owner'] == self.address:
+                amount_0, amount_1, tick_lower, tick_upper, liquidity = (record['amount0'], record['amount1'],
+                                                                         record['tick_lower'], record['tick_upper'],
+                                                                         record['liquidity'])
+                self.perform_mint(portfolio, amount_0, amount_1, tick_lower, tick_upper, liquidity)
+                is_rebalanced = 'mint'
+
+        if event == 'burn':
+            if record['owner'] == self.address:
+                amount_0, amount_1, tick_lower, tick_upper, liquidity, price = (
+                record['amount0'], record['amount1'],
+                record['tick_lower'], record['tick_upper'],
+                record['liquidity'], record['price'])
+                self.perform_burn(portfolio, amount_0, amount_1, tick_lower, tick_upper, liquidity, price)
+                is_rebalanced = 'burn'
+
+        if event == 'swap':
+            if record['owner'] == self.address:
+                amount_0, amount_1 = record['amount0'], record['amount1']
+                self.perform_swap(portfolio, amount_0, amount_1)
+                is_rebalanced = 'swap'
+
+        if event == 'swap':
+            price_before, price = record['price_before'], record['price']
+            for name, pos in portfolio.positions.items():
+                if 'Uni' in name:
+                    pos.charge_fees(price_before, price)
+
+        self.perform_clearing(portfolio)
+        return is_rebalanced
+
+    def perform_swap(self, portfolio, amount_0, amount_1):
+        vault = portfolio.get_position('Vault')
+        if amount_0 > 0:
+            if vault.x < amount_0:
+                vault.deposit(amount_0 - vault.x, 0)
+            vault.withdraw(amount_0, 0)
+            vault.deposit(0, -amount_1)
+        else:
+            if vault.y < amount_1:
+                vault.deposit(0, amount_1 - vault.y)
+            vault.withdraw(0, amount_1)
+            vault.deposit(-amount_0, 0)
+
+    def perform_mint(self, portfolio, amount_0, amount_1, tick_lower, tick_upper, liquidity):
+        name = f'UniV3_{tick_lower}_{tick_upper}'
+        vault = portfolio.get_position('Vault')
+
+        if vault.x < amount_0:
+            vault.deposit(amount_0 - vault.x, 0)
+
+        if vault.y < amount_1:
+            vault.deposit(0, amount_1 - vault.y)
+
+        x_uni, y_uni = vault.withdraw(amount_0, amount_1)
+
+        price_lower, price_upper = self._tick_to_price(tick_lower), self._tick_to_price(tick_upper)
+
+        if name in portfolio.positions:
+            univ3_pos_old = portfolio.get_position(name)
+            univ3_pos_old.liquidity = univ3_pos_old.liquidity + liquidity
+            univ3_pos_old.bi_currency.deposit(amount_0, amount_1)
+        else:
+            univ3_pos = UniV3Position(name, price_lower, price_upper, self.fee_percent, self.rebalance_cost)
+            univ3_pos.liquidity = liquidity
+            univ3_pos.bi_currency.deposit(amount_0, amount_1)
+            portfolio.append(univ3_pos)
+
+    def perform_burn(self, portfolio, amount_0, amount_1, tick_lower, tick_upper, liquidity, price):
+        name = f'UniV3_{tick_lower}_{tick_upper}'
+
+        if name in portfolio.positions:
+            univ3_pos_old = portfolio.get_position(name)
+            vault = portfolio.get_position('Vault')
+            vault.deposit(amount_0, amount_1)
+
+            if liquidity > 0:
+                if liquidity > univ3_pos_old.liquidity:
+                    print('Diff =', liquidity - univ3_pos_old.liquidity)
+                    x_out, y_out = univ3_pos_old.burn(univ3_pos_old.liquidity, price)
+                else:
+                    x_out, y_out = univ3_pos_old.burn(liquidity, price)
+            else:
+                print(f'Negative liq {liquidity}')
+        else:
+            print(f'There is no position to burn {name}')
+
+    def perform_clearing(self, portfolio):
+        poses = copy.copy(portfolio.positions)
+        for name, pos in poses.items():
+            if 'UniV3' in name:
+                univ3 = portfolio.get_position(name)
+                if univ3.liquidity < 1e1:
+                    portfolio.remove(name)
+
+    def _tick_to_price(self, tick):
+        price = np.power(1.0001, tick) / 10 ** self.decimal_diff
+        return price